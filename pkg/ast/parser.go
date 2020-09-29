921ef13e9427ee9eba6760a0b59712aaae1d6e30
Scott Meeuwsen <scott@apisw.com>
2020-09-28 23:07:14 -0400

    Add AST printing and parser test

--- pkg/ast/parser.go
+++ pkg/ast/parser.go
@@ -1,23 +1,43 @@
 package ast
 
 import (
+	"fmt"
 	"log"
+	"strings"
 
 	"github.com/nu11ptr/parsegen/pkg/lexer"
 	"github.com/nu11ptr/parsegen/pkg/token"
 )
 
+const spaces = 3
+
 type TopLevel struct {
-	ParserRules map[string]*ParserRule
-	LexerRules  map[string]*LexerRule
+	ParserRulesMap map[string]*ParserRule
+	LexerRulesMap  map[string]*LexerRule
+
+	ParserRules []*ParserRule
+	LexerRules  []*LexerRule
 }
 
 func NewTopLevel(rules []*ParserRule) *TopLevel {
-	topLevel := new(TopLevel)
+	topLevel := &TopLevel{
+		ParserRulesMap: make(map[string]*ParserRule, 16),
+		LexerRulesMap:  make(map[string]*LexerRule, 16),
+	}
 	for _, rule := range rules {
-		topLevel.ParserRules[rule.Name] = rule
+		topLevel.ParserRulesMap[rule.Name] = rule
+		topLevel.ParserRules = append(topLevel.ParserRules, rule)
 	}
 	return topLevel
+}
+
+func (t *TopLevel) String() string {
+	buff := strings.Builder{}
+	buff.WriteString("TopLevel:\n")
+	for _, rule := range t.ParserRules {
+		buff.WriteString(rule.String(1))
+	}
+	return buff.String()
 }
 
 type ParserRule struct {
@@ -25,11 +45,23 @@ type ParserRule struct {
 	Rules *ParserAlternatives
 }
 
+func (p *ParserRule) String(indent int) string {
+	buff := strings.Builder{}
+	buff.WriteString(strings.Repeat(" ", indent*spaces))
+	buff.WriteString(fmt.Sprintf("└──ParserRule: %s\n", p.Name))
+	buff.WriteString(p.Rules.String(indent + 1))
+	return buff.String()
+}
+
 type ParserNode interface {
 	ParserNode()
+	String(int) string
 }
 
 func NewNestedNode(node ParserNode, suffix *lexer.Token) ParserNode {
+	if suffix == nil {
+		return node
+	}
 	switch suffix.Type {
 	case token.PLUS:
 		return &ParserOneOrMore{Node: node}
@@ -47,10 +79,33 @@ type ParserAlternatives struct {
 	Rules [][]ParserNode
 }
 
+func (p *ParserAlternatives) String(indent int) string {
+	buff := strings.Builder{}
+	buff.WriteString(strings.Repeat(" ", indent*spaces))
+	buff.WriteString("└──Alternatives:\n")
+
+	for i, alt := range p.Rules {
+		buff.WriteString(strings.Repeat(" ", (indent+1)*spaces))
+		buff.WriteString(fmt.Sprintf("└──Alternative %d:\n", i))
+		for _, rule := range alt {
+			buff.WriteString(rule.String(indent + 2))
+		}
+	}
+	return buff.String()
+}
+
 func (p *ParserAlternatives) ParserNode() {}
 
 type ParserZeroOrMore struct {
 	Node ParserNode
+}
+
+func (p *ParserZeroOrMore) String(indent int) string {
+	buff := strings.Builder{}
+	buff.WriteString(strings.Repeat(" ", indent*spaces))
+	buff.WriteString("└──ZeroOrMore:\n")
+	buff.WriteString(p.Node.String(indent + 1))
+	return buff.String()
 }
 
 func (p *ParserZeroOrMore) ParserNode() {}
@@ -59,10 +114,26 @@ type ParserOneOrMore struct {
 	Node ParserNode
 }
 
+func (p *ParserOneOrMore) String(indent int) string {
+	buff := strings.Builder{}
+	buff.WriteString(strings.Repeat(" ", indent*spaces))
+	buff.WriteString("└──OneOrMore:\n")
+	buff.WriteString(p.Node.String(indent + 1))
+	return buff.String()
+}
+
 func (p *ParserOneOrMore) ParserNode() {}
 
 type ParserZeroOrOne struct {
 	Node ParserNode
+}
+
+func (p *ParserZeroOrOne) String(indent int) string {
+	buff := strings.Builder{}
+	buff.WriteString(strings.Repeat(" ", indent*spaces))
+	buff.WriteString("└──ZeroOrOne:\n")
+	buff.WriteString(p.Node.String(indent + 1))
+	return buff.String()
 }
 
 func (p *ParserZeroOrOne) ParserNode() {}
@@ -71,16 +142,41 @@ type ParserRuleRef struct {
 	Name string
 }
 
+func (p *ParserRuleRef) String(indent int) string {
+	buff := strings.Builder{}
+	buff.WriteString(strings.Repeat(" ", indent*spaces))
+	buff.WriteString(fmt.Sprintf("└──ParserRuleRef: %s\n", p.Name))
+	return buff.String()
+}
+
 func (p *ParserRuleRef) ParserNode() {}
 
 type ParserLexerRuleRef struct {
 	Name string
 }
 
+func (p *ParserLexerRuleRef) String(indent int) string {
+	buff := strings.Builder{}
+	buff.WriteString(strings.Repeat(" ", indent*spaces))
+	buff.WriteString(fmt.Sprintf("└──LexerRuleRef: %s\n", p.Name))
+	return buff.String()
+}
+
 func (p *ParserLexerRuleRef) ParserNode() {}
 
 type ParserToken struct {
 	Token *lexer.Token
+}
+
+func (p *ParserToken) String(indent int) string {
+	buff := strings.Builder{}
+	buff.WriteString(strings.Repeat(" ", indent*spaces))
+	buff.WriteString("└──Token Literal:\n")
+	if p.Token.Data != "" {
+		buff.WriteString(strings.Repeat(" ", (indent+1)*spaces))
+		buff.WriteString(fmt.Sprintf("└──Data: %s\n", p.Token.Data))
+	}
+	return buff.String()
 }
 
 func (p *ParserToken) ParserNode() {}
---
+++ pkg/parser/parser_test.go
@@ -0,0 +1,107 @@
+package parser_test
+
+import (
+	"testing"
+
+	"github.com/nu11ptr/parsegen/pkg/lexer"
+	"github.com/nu11ptr/parsegen/pkg/parser"
+	"github.com/nu11ptr/parsegen/pkg/token"
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	grammar = `top_level: (parse_rule | lex_rule)* EOF;
+
+// *** Parser parser ***
+
+parse_rule: RULE_NAME ':' rule_body ';';
+
+rule_body: rule_sect+ ('|' rule_sect+)*;
+
+rule_sect: rule_part suffix?;
+
+rule_part
+	: '(' rule_body ')'
+	| RULE_NAME
+	| TOKEN_NAME
+	| TOKEN_LIT
+	;
+
+suffix: '+' | '*' | '?';
+`
+
+	expected = `TopLevel:
+   └──ParserRule: top_level
+      └──Alternatives:
+         └──Alternative 0:
+            └──ZeroOrMore:
+               └──Alternatives:
+                  └──Alternative 0:
+                     └──ParserRuleRef: parse_rule
+                  └──Alternative 1:
+                     └──ParserRuleRef: lex_rule
+            └──LexerRuleRef: EOF
+   └──ParserRule: parse_rule
+      └──Alternatives:
+         └──Alternative 0:
+            └──LexerRuleRef: RULE_NAME
+            └──Token Literal:
+               └──Data: ':'
+            └──ParserRuleRef: rule_body
+            └──Token Literal:
+               └──Data: ';'
+   └──ParserRule: rule_body
+      └──Alternatives:
+         └──Alternative 0:
+            └──OneOrMore:
+               └──ParserRuleRef: rule_sect
+            └──ZeroOrMore:
+               └──Alternatives:
+                  └──Alternative 0:
+                     └──Token Literal:
+                        └──Data: '|'
+                     └──OneOrMore:
+                        └──ParserRuleRef: rule_sect
+   └──ParserRule: rule_sect
+      └──Alternatives:
+         └──Alternative 0:
+            └──ParserRuleRef: rule_part
+            └──ZeroOrOne:
+               └──ParserRuleRef: suffix
+   └──ParserRule: rule_part
+      └──Alternatives:
+         └──Alternative 0:
+            └──Token Literal:
+               └──Data: '('
+            └──ParserRuleRef: rule_body
+            └──Token Literal:
+               └──Data: ')'
+         └──Alternative 1:
+            └──LexerRuleRef: RULE_NAME
+         └──Alternative 2:
+            └──LexerRuleRef: TOKEN_NAME
+         └──Alternative 3:
+            └──LexerRuleRef: TOKEN_LIT
+   └──ParserRule: suffix
+      └──Alternatives:
+         └──Alternative 0:
+            └──Token Literal:
+               └──Data: '+'
+         └──Alternative 1:
+            └──Token Literal:
+               └──Data: '*'
+         └──Alternative 2:
+            └──Token Literal:
+               └──Data: '?'
+`
+)
+
+func TestParser(t *testing.T) {
+	lex := lexer.NewFromString(grammar)
+	tokenizer := token.NewParseGen(lex)
+	parse := parser.NewParser(tokenizer)
+	parsegen := parser.NewParseGen(parse)
+
+	ast := parsegen.ParseTopLevel()
+	assert.Equal(t, expected, ast.String())
+}
